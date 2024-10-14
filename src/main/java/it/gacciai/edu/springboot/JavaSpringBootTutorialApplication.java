1977cc5b5d8614f90c1c8e122b3196f9c090e36c
Giovanni Acciai <g.acciai@sintraconsulting.eu>
2024-10-14 09:28:56 +0200

    Created project

---
+++ src/main/java/it/gacciai/edu/springboot/JavaSpringBootTutorialApplication.java
@@ -0,0 +1,13 @@
+package it.gacciai.edu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gacciai/edu/springboot/JavaSpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gacciai.edu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
